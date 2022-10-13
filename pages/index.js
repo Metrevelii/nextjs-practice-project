569991803f06e7163dcf729ac06030b870fb6127
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-10-13 23:01:18 +0400

    added starter files

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import NewNav from '../pages/navigation/nav';
+
+export default function Home() {
+  return (
+    <NewNav />
+  )
+}
---
+++ pages/navigation/nav.js
@@ -0,0 +1,14 @@
+const NewNav = () => {
+    return (
+        <div class="flex justify-center m-auto w-96">
+            <nav class="flex space-around">
+                <li>Home</li>
+                <li>Members</li>
+                <li>Gallery</li>
+                <li>History</li>
+            </nav>
+        </div>
+    )
+}
+
+export default NewNav;
